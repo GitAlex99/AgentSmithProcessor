4deee8536b8ace2b3c3f738b577fe212d2c113b1
Alex <alex.bartoli@outlook.com>
2025-12-25 18:19:08 +0100

    implemented dlt listener

--- src/main/java/com/smith/processor/config/KafkaProducerConfig.java
+++ src/main/java/com/smith/processor/config/KafkaProducerConfig.java
@@ -1,6 +1,7 @@
 package com.smith.processor.config;
 
 import com.smith.processor.dto.EventDTO;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -8,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -23,9 +25,12 @@ public class KafkaProducerConfig {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG,true);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, EventDTO.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
 
@@ -45,4 +50,22 @@ public class KafkaProducerConfig {
         return factory;
     }
 
+    @Bean
+    public ProducerFactory<Object, Object> retryProducerFactory() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<Object, Object> retryKafkaTemplate() {
+        return new KafkaTemplate<>(retryProducerFactory());
+    }
 }
--- src/main/java/com/smith/processor/listener/EventListener.java
+++ src/main/java/com/smith/processor/listener/EventListener.java
@@ -3,7 +3,13 @@ package com.smith.processor.listener;
 import com.smith.processor.dto.EventDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,8 +17,32 @@ public class EventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(EventListener.class);
 
+
+    @RetryableTopic(
+            attempts = "3",
+            backoff = @Backoff(delay = 1000,multiplier = 2.0),
+            autoCreateTopics = "true",
+            dltStrategy = DltStrategy.FAIL_ON_ERROR,
+            kafkaTemplate = "retryKafkaTemplate"
+    )
     @KafkaListener(topics = "smith.events.ingestion.v1", groupId = "test")
     public void eventListener(EventDTO event){
+
         logger.info("message received: {}", event);
+
+        if(!event.getSeverity().equals("test")){
+            throw new RuntimeException();
+        }
+        logger.info("message correctly processed: {}", event);
+
+    }
+
+    @KafkaListener(topics = "smith.events.ingestion.v1-dlt", groupId = "test")
+    public void eventListenerDlt(EventDTO event,
+                                 @Header(value = KafkaHeaders.EXCEPTION_STACKTRACE, required = false) String exceptionMessage,
+                                 @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                                 @Header(KafkaHeaders.OFFSET) long offset) {
+
+        logger.info("message received in dlt listener: event: {},topic: {},offset: {}, error: {}", event, topic, offset, exceptionMessage );
     }
 }
